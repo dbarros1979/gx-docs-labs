140148176761ef94b0161550cd7114f628387398
dbarros1979 <daniel.barros@genesis.global>
2023-05-24 14:56:38 +0000

    Improving python script

--- data/mds2csv.py
+++ data/mds2csv.py
@@ -1,22 +1,38 @@
-import requests
+import os
 import csv
+import codecs
 
-url = "https://api.github.com/repos/dbarros1979/gx-docs-labs/contents"
-response = requests.get(url)
-data = response.json()
+# Change directory to the parent directory
+os.chdir('.')
+dir = os.getcwd()
 
-with open('allsite.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Topic", "Line"])
-    for item in data:
-        if item["type"] == "file" and item["name"].endswith(".md"):
-            url = item["url"]
-            response = requests.get(url)
-            content = response.json()["content"]
-            decoded_content = base64.b64decode(content).decode("utf-8")
-            lines = decoded_content.splitlines()
-            for line in lines:
-                if line.startswith("#"):
-                    topic = line.strip("#").strip()
-                else:
-                    writer.writerow([topic, line])
+data = [["prompt", "raw_data"]]
+# r = root, d = directories, f = files
+for r, d, f in os.walk(dir):
+    for file_name in f:
+        if file_name.endswith('.md'):
+            with codecs.open(os.path.join(r, file_name), "r", encoding="utf-8") as file:
+                #file_content = base64.b64decode(file.read()).decode("utf-8")
+                file_content = file.read()
+                if "#" in file_content:
+                    prompt = ""
+                    raw_data = ""
+                    lines = file_content.splitlines()
+                    for line in lines:
+                        if "#" in line:
+                            if len(prompt) > 0:
+                                data.append([{prompt}, {raw_data}])
+                                raw_data = ""
+                            prompt = line.replace("#", "")
+                        elif len(prompt) > 0:
+                            raw_data += line
+                    if len(prompt) > 0 and prompt not in data[0]:
+                        data.append([{prompt}, {raw_data}])
+
+# Get the path of the current directory
+output_file = os.path.dirname(os.path.realpath(__file__)) + "\output.csv"
+with codecs.open(output_file, "w", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
+
+
